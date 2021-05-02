90f7e057d4f76d1713a0c10a4beb61a5e65d8021
Swarit Shrivastava <73647160+SwaritShrivastava@users.noreply.github.com>
2021-05-02 09:53:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+mustacheX=0;
+mustacheY=0;
+function preload() {
+    mustache= loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgqvkeECN9qxYtkJCEHhs5HsV_ZqRGx5l2bw&usqp=CAU');
+}
+function setup() {
+canvas = createCanvas(300, 300);
+canvas.center();
+}
